70d32f84f68d1e944b2cb9e5ad494b5c7e91e2cf
hiro9108 <hiroshi.8.egawa@gmail.com>
2020-10-13 18:07:53 -0700

    feat: add the Button component with material-ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,17 @@
 import React, { FC } from 'react';
 import Link from 'next/link';
+import Router from 'next/router';
 
-import styles from './index.module.css';
+import styles from './styles.module.css';
+import Button from '../components/Button';
 
 const topIndexPage: FC = () => (
     <div>
         <div className={styles.Header}>Backend Languages</div>
-        <div className={styles.Element}><p><Link href="/python">Go to Python</Link></p></div>
-        <div className={styles.Element}><p><Link href="/nodejs">Go to Nodejs</Link></p></div>
-        <div className={styles.Element}><p><Link href="/golang">Go to Golang</Link></p></div>
+        <div className={styles.Element}><p><Link href='/python'>Go to Python</Link></p></div>
+        <div className={styles.Element}><p><Link href='/nodejs'>Go to Nodejs</Link></p></div>
+        <div className={styles.Element}><p><Link href='/golang'>Go to Golang</Link></p></div>
+        <Button clicked={() => Router.push('/golang')}>Router</Button>
     </div>
 );
 
--- pages/nodejs/index.tsx
+++ pages/nodejs/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Link from 'next/link';
 
-import styles from './index.module.css';
+import styles from '../styles.module.css';
 
 const pythonIndexPage = () => (
     <div className={styles.App}>
